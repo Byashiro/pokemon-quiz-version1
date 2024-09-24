c992aa70fb66ed66c6ff60e832b9fa5cea997b2a
Brandon Andres Garcia Guzman <bgarciag@frd.org.mx>
2024-09-24 11:12:23 -0600

    ocultacion codigo fuente

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,13 @@ const { defineConfig } = require('@vue/cli-service');
 const webpack = require('webpack');
 
 module.exports = defineConfig({
+  outputDir: 'dist',
   transpileDependencies: true,
   configureWebpack: {
+    mode: 'production',
+    optimization: {
+      minimize: true
+    },
     plugins: [
       new webpack.DefinePlugin({
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true'
